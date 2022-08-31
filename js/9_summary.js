67d52376d79d4c582878a411087208dc05dcefab
arfan36 <arfanur36@gmail.com>
2022-08-31 12:33:42 +0600

    34-9 Module Summary and recap

---
+++ js/9_summary.js
@@ -0,0 +1,28 @@
+/* 
+FETCH
+1. fetch must provite url
+    dynamic or  static
+2. how to convert fetch promise return to json
+3. how to convert to json to data
+4. [cool headed]:
+    users:
+    --> array of objects
+    --> objects with properties users
+    --> object with properties called data
+
+nested object
+
+
+------------------------------
+DOM manipulation
+1. get something from the DOM
+2. create element to append to the DOM
+3. dynamically load data based on id
+
+------------------------------
+array --> forEach, map, find, filter
+-----------------------------------
+template string
+
+
+ */
